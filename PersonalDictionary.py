efdd8634bb30798e31b2656c7414dc25a1dc6935
Adam Czepielik <adam.czepielik@gmail.com>
2021-06-08 19:37:56 +0200

    Added basic caching

--- PersonalDictionary.py
+++ PersonalDictionary.py
@@ -77,12 +77,18 @@ class DictionaryConnection:
         'Thesaurus': 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/'
     }
 
-    def __init__(self, output_dir=os.path.join(os.environ["HOME"], '.personaldictionary')):
+    def __init__(self, dir=os.path.join(os.environ["HOME"], '.personaldictionary')):
+        api_config_path = os.path.join(dir, ".keys")
+        self.meta_dict_path = os.path.join(dir, 'meta.json')
+
         api_config = configparser.ConfigParser()
-        api_config.read(".keys")
+        api_config.read(api_config_path)
 
         self.config = api_config
-        self.outdir = output_dir
+        self.dir = dir
+
+        with open(self.meta_dict_path, 'r') as file:
+            self.meta_dict = json.load(file)
 
     def _check_dictionary(self, word, endpoint='Dictionary'):
         url = self.endpoints[endpoint]
@@ -98,10 +104,55 @@ class DictionaryConnection:
 
         return parsed
 
-    def _save_word(self, response):
-        pass
+    def _check_cache(self, word):
+        if word in self.meta_dict:
+            entry_file= self.meta_dict.get(word)
+            entry_path = os.path.join(self.dir, entry_file)
 
-    def check_word(self, word, force=False):
-        pass
+            with open(entry_path, 'r') as file:
+                entry = json.load(file)
+
+            return entry
+        else:
+            return None
+
+    def _save_word(self, word, entry):
+        word_path = os.path.join(self.dir, ".data", word + '.json')
+
+        with open(word_path, 'w+') as file:
+            json.dump(entry, file)
+            
+        self.meta_dict[word] = os.path.join(".data", word + '.json')
+
+        with open(self.meta_dict_path, 'w+') as meta_file:
+            json.dump(self.meta_dict, meta_file)
+
+    def check_word(self, word, force=False, prompt=True, save=True):
+
+        if not force:
+            entry = self._check_cache(word)
+            from_cache = True
+        else:
+            entry = None
+        
+        if entry is None:
+            entry = self._check_dictionary(word)
+            from_cache = False
+
+        if prompt:
+            print(entry)
+        
+        if save and not from_cache:
+            self._save_word(word, entry)
+
+if __name__ == "__main__":
+    
+    MW = DictionaryConnection()
+
+    MW.check_word('voluminous')
+    MW.check_word('voluminous')
+
+
+
 
 
